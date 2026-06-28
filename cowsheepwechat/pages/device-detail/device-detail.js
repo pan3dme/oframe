3e570b20c5863b3822f4396c077010ac9e3fe2af
pan3dme <3423526@qq.com>
2026-06-28 21:35:59 +0800

    33

--- cowsheepwechat/pages/device-detail/device-detail.js
+++ cowsheepwechat/pages/device-detail/device-detail.js
@@ -166,6 +166,7 @@ Page({
       data: {
         action: 'getDeviceLogbyId',
         info: {
+          limit:30,
           deviceId: deviceId,
           curdate: this.data.trackDate
         },
@@ -420,6 +421,7 @@ Page({
       data: {
         action: 'getDeviceLogbyId',
         info: {
+          limit:20,
           deviceId: deviceId,
           curdate: this.data.trackDate
         },
